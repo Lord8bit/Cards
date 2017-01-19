e53851b3dffa41e09f64d73cd69e5e955a2d713e
John Doe <jonahdefez@gmail.com>
2017-01-19 09:25:40 -0700

    CHanged

--- CardTester.java
+++ CardTester.java
@@ -1,8 +1,16 @@
 
 public class CardTester {
 
-
+	
 	public static void main(String[] args) {
-		
+		Card card1 = new Card("Jack", "Hearts", 10);
+		Card card2 = new Card("Murloc", "Azeroth", 1);
+		Card card3 = new Card("Worgen", "Azeroth", 1);
+		Card card4 = new Card("Black Lotus", "Artifact", 0);
+		System.out.println(card4);
+		System.out.println(card3);
+		System.out.println(card2);
+		System.out.println(card1);
+		System.out.println(card3.someMatches(card2));
 	}
 }
